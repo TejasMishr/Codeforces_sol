f0a1d8e240bfe81557e12ad62bf698f708eccb17
TejasMishr <tejasmishra72@gmail.com>
2023-09-21 13:09:42 +0530

    stack linked list implementation

---
+++ stack/linkedlist.cpp
@@ -0,0 +1,69 @@
+// Header Files and namespaces
+#include<bits/stdc++.h>
+using namespace std;
+// Code Shortners
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define pb push_back
+#define mp make_ pair
+#define len(s) (int)s.size()
+#define print(x) cout<<x<<'\n'
+#define REP(i,a,b) for ( int i = a; i< b; i++)
+#define all(a) (a). begin(), (a).end()
+#define fast ios_bfse::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define mod 1000000007;
+
+
+struct node{
+    int data;
+    struct node *next;
+}
+*top=NULL;
+void push(int val){
+     struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data=val;
+    if(top==NULL) newnode->next=NULL;
+    else newnode->next=top;
+
+    top=newnode;
+    cout<<"Successful Insertion"<<endl;
+    
+}
+void pop(){
+    if (top==NULL) cout<<"Empty Stack"<<endl;
+    else{
+        struct node *temp=top;
+        cout<<"Element deleted: "<<temp->data<<endl;
+        top=temp->next;
+        free(temp);
+       
+    }
+}
+
+void display(){
+    if (top==NULL) cout<<"Empty Stack"<<endl;
+    struct node *temp=top;
+    while(temp->next!=NULL) {
+        cout<<"data : "<<temp->data<<endl;
+        temp=temp->next;
+    }
+    cout<<"data : "<<temp->data<<endl;
+
+}
+
+
+int main (){
+    
+     push(1);
+     push(2);
+     push(4);
+     push(5);
+     display();
+     pop();
+     display();
+  
+  return 0;
+}
