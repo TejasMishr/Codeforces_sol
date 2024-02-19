3d20f2f54f21199d371e4f594c11ea291c3808d4
TejasMishr <tejasmishra72@gmail.com>
2024-02-19 17:48:22 +0530

    string Hashing

---
+++ Codeforces/String_Hashing/stringHash.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9 + 7;
+
+long long get_hash(string s){
+    long long h=0;
+    for(auto c : s) h = ( h * 31 + (c - 'a' + 1)) % MOD;
+    return h;
+}
+
+int main(){
+    cout<<get_hash("ab");
+}
