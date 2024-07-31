336fc17323b1f0a2978fc1e2a039d836aac38a71
TejasMishr <tejasmishra72@gmail.com>
2024-07-31 22:31:59 +0530

    ok

---
+++ D_Product_of_Binary_Decimals.cpp
@@ -0,0 +1,72 @@
+// +########################################################################################+
+#include<bits/stdc++.h>
+using namespace std;
+// Code Shortners
+#define ub upper_bound
+#define lb lower_bound
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vll vector<ll>
+#define vvi vector<vi>
+#define vvc vector<vector<char>>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll,ll>>
+#define pii pair<int,int>
+#define mll map<ll,ll>
+#define pll pair<ll, ll>
+#define F first
+#define S second
+#define pb push_back
+#define ppb pop_back
+#define pf  push_front
+#define ppf pop_front
+#define mpair make_pair
+#define len(s) (int)s.size()
+#define inp(i,a,b,vec) for(ll i = a; i < b; i++) cin>>vec[i];
+#define print(x) cout<<x<<endl;
+#define REP(i,a,b) for(ll i = a; i < b; i++)
+#define REPE(i,a,b) for(ll i = a; i <= b; i++)
+#define nl cout<<endl;
+#define autoprint(a) for(auto i:a) cout<<i<<nl;
+#define trav(x,a) for(auto& x : a)
+//  Container bounds
+#define rall(c)rbegin(c),rend(c)
+#define all(a) (a).begin(), (a).end()
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define SUM(v) accumulate(all(v),0)
+#define UNIQUE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define fast ios_bfse::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define mod 1000000007
+#define si set<int>
+#define sc set<char>
+#define sst set<string>
+#define Si stack<int>
+#define Sc stack<char>
+#define Sst stack<string>
+ #define readll(n) ll n; cin >> n
+#define readS(str) string str;  cin >> str
+
+//---------------------------------------------------------------------------------------------------------------------
+void sol(){
+  int n; cin >> n;
+  for(int i = 32; i>1; i--) {
+
+    string s = bitset< 6 >( i ).to_string();
+    int temp=stoi(s);
+
+    while( n % temp == 0) n /= temp;
+  }
+  string v= (n==1) ? "YES" : "NO";
+  print(v);
+}
+int main(){
+  readll(t);
+  while(t--){
+      sol();
+  }
+  return 0;
+}
